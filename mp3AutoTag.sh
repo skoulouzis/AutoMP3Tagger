035fd8c60f83287204982af79c68d05bb629662b
skoulouzis <S.Koulouzis@uva.nl>
2017-03-21 17:58:47 +0100

    clean html tags from snipet to better look for mand name

--- mp3AutoTag.sh
+++ mp3AutoTag.sh
@@ -27,18 +27,12 @@ urldecode() {
 }
 
 
-IFS=$'\n'
-MUSIC=/media/$USER/zapp/music/Ramones/
-
-
-for f in $(find $MUSIC -type f); do
-    title=`ffprobe -loglevel error -show_entries format_tags=title -of default=noprint_wrappers=1:nokey=1 $f`
-    artist=`ffprobe -loglevel error -show_entries format_tags=artist -of default=noprint_wrappers=1:nokey=1 $f`
-    info=$artist" "$title
-    encoded=`urlencode $info`
+search(){
+    encoded=$1
+#     echo "https://en.wikipedia.org/w/api.php?action=query&format=json&redirects&list=search&srlimit=1&srsearch=$encoded"
     curl "https://en.wikipedia.org/w/api.php?action=query&format=json&redirects&list=search&srlimit=1&srsearch=$encoded"  > title.json
     title=`jq .query.search[0].title title.json`
-    snipet=`jq .query.search[0].snippet title.json | tr '[:upper:]' '[:lower:]'`
+    snipet=`jq .query.search[0].snippet title.json | sed -e 's/<\/b>/-/g' -e 's/<[^>]*>//g'  | tr '[:upper:]' '[:lower:]'`
     artist=`echo $artist | tr '[:upper:]' '[:lower:]'`
     if [[ $snipet == *$artist* ]]; then
         title=`sed -e 's/^"//' -e 's/"$//' <<<"$title"`
@@ -49,6 +43,7 @@ for f in $(find $MUSIC -type f); do
         pageID=`sed -e 's/^"//' -e 's/"$//' <<<"${arr[0]}"`
         k=`jq keys[] pages.json`
         extract=`jq .$k.extract pages.json | tr '[:upper:]' '[:lower:]'`
+        
         if [[ $extract == *"song"* ]]; then
             curl "https://en.wikipedia.org/?curid=$pageID" | hxnormalize -x > $pageID.html
             xpath -e '//tr[position()>1]' $pageID.html 2> /dev/null | sed -e 's/<\/*tr>//g' -e 's/<td>//g' -e 's/<\/td>/ /g' | awk 'NF' > $pageID.out
@@ -65,20 +60,41 @@ for f in $(find $MUSIC -type f); do
                         startLooking=true
                     fi
                     if [[ $p == *"title=\""* ]] && [ "$startLooking" = true ] && [[ $p != *"Music genre"* ]]; then
-                        genre+=`echo $p | grep -o -P '(?<=title\=\")(.*?)(?=\")'`"/"
+                        split=`echo $p | grep -o -P '(?<=title\=\")(.*?)(?=\")' | tr '\n' '\/'`
+                        split=`urldecode $split`
+                        split=`echo $split | sed 's/\&amp;/\&/g'`
+                        genre+=$split
                     fi
                 done < $pageID.out
-                    
-                if [ -n "$genre" ]; then
-                    genre=`echo "${genre::-1}"`
-                    echo $f
-                    echo $genre
-                    mid3v2 --genre="$genre" $f
-                fi
             fi
         fi
     fi
-    rm *.json
-done
+}
 
+
+IFS=$'\n'
+MUSIC=/media/$USER/zapp/music/Orbital
+
+
+for f in $(find $MUSIC -type f -iname "*.mp3"); do
+#     echo $f
+    title=`ffprobe -loglevel error -show_entries format_tags=title -of default=noprint_wrappers=1:nokey=1 $f`
+    artist=`ffprobe -loglevel error -show_entries format_tags=artist -of default=noprint_wrappers=1:nokey=1 $f`
+    info=$artist" "$title
+#     echo $info
+    encoded=`urlencode $info`
+
+    genre=""
+    search $encoded 
+    if [ -n "$genre" ]; then
+        genre=`echo "${genre::-1}"`
+        echo $artist $title
+        echo $f
+        echo $genre
+        mid3v2 --genre="$genre" $f
+    fi
+    genre=""
+    rm *.html
+done
+rm *.json
 rm *.out
